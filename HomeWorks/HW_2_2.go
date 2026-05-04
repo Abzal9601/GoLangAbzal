c34e2636b4fadd618438e6007ac50b477bf1d017
Abzal9601 <abzalyeleu030401@gmail.com>
2026-05-04 17:13:15 +0500

    3rd homework

---
+++ HomeWorks/HW_2_2.go
@@ -0,0 +1,79 @@
+package main
+
+//1st exercise
+
+// func main() {
+
+// 	age := 25
+
+// 	fmt.Println(age)
+
+// 	age = age + 3
+
+// 	fmt.Println(age)
+// }
+
+//2nd exercise
+
+// func main() {
+
+// 	var height int
+
+// 	height = 176
+
+// 	fmt.Println(height)
+
+// 	var height_in_meters float64
+
+// 	height_in_meters = 1.76
+
+// 	fmt.Println(height_in_meters)
+// }
+
+// //3rd exercise
+
+// func main() {
+
+// 	var isStunedt bool
+
+// 	isStunedt = true
+
+// 	fmt.Println(isStunedt)
+// }
+
+//4th exercise (чуть чуть усложнил задачу )
+
+// func main() {
+
+// 	temperature := 15
+
+// 	if temperature > 20 {
+
+// 		fmt.Println("Погода сегодня теплая")
+
+// 	} else {
+// 		fmt.Println("Погода сегодня прохладная")
+// 	}
+// }
+
+// //5th exercise
+
+// func main() {
+
+// 	favoriteQuote := "\"Легче заработать деньги, чем найти настояющую любовь.\""
+
+// 	fmt.Println(favoriteQuote)
+// }
+
+// //6th exercise
+
+// func main() {
+
+// 	pi := 3.14
+
+// 	fmt.Println(pi)
+
+// 	pi = "3.1456"
+
+// 	fmt.Printf(pi)  (невозможно так как при создании переменной переменной был присвоен тип FLOAT. )
+// }
