04745d3465cb401dc564f10461fa7e4c9608651d
Abzal9601 <abzalyeleu030401@gmail.com>
2026-05-04 15:00:02 +0500

    FirstHomeWork

---
+++ HomeWorks/HW_4_1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// func main() {
+
+// 	fmt.Println("Я сегодня изучил:")
+// }
+
+// func main() {
+
+// 	fmt.Println("Основы языка Go")
+
+// }
+
+func main() {
+
+	fmt.Println("Мой любиый исполнитель Михаил Боярский")
+}
