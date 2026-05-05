09358cc55e942dc5393a5c4798abc44a95ade542
Abzal9601 <abzalyeleu030401@gmail.com>
2026-05-05 14:27:53 +0500

    4th home work

---
+++ HomeWorks/HW_2_4.go
@@ -0,0 +1,127 @@
+package main
+
+//1st exercise
+
+// func main() {
+
+// 	bannerWidth := 12
+
+// 	bannerHeight := 8
+
+// 	bannerArea := bannerHeight * bannerWidth
+
+// 	fmt.Println(bannerArea)
+
+// 	halfBannerArea := bannerArea / 2
+
+// 	fmt.Println(halfBannerArea)
+
+// 	bannerBorderLength := (bannerWidth + bannerHeight) * 2
+
+// 	fmt.Println(bannerBorderLength)
+
+// }
+
+//2nd exercise
+
+// func main() {
+
+// 	boxCount := 29
+
+// 	leftoverBoxes := boxCount % 5
+
+// 	fmt.Println(leftoverBoxes)
+
+// }
+
+//3rd Exercise
+// func main() {
+
+// 	tempMorning := 15
+// 	tempAfternoon := 23.5
+// 	tempEvening := 30
+
+// 	totalTemp := float64(tempMorning) + tempAfternoon + float64(tempEvening)
+
+// 	averageTemp := totalTemp / 3
+
+// 	fmt.Println(averageTemp)
+
+// }
+
+//4th exercise
+
+// func main() {
+
+// 	knownWords := 47
+// 	wordsGoal := 120
+
+// 	progressPercent := float64(knownWords) / float64(wordsGoal) * 100
+
+// 	fmt.Println(progressPercent, "%")
+// }
+
+// //5th exercise
+
+// // func main() {
+
+// // 	coins := 0
+
+// fmt.Println(coins)
+
+// // 	coins += 500
+
+// fmt.Println(coins)
+
+// // 	coins += 1200
+
+// fmt.Println(coins)
+
+// // 	coins /= 2
+
+// fmt.Println(coins)
+
+// // 	coins *= 2
+
+// fmt.Println(coins)
+
+// // 	coins -= 300
+
+// // 	fmt.Println(coins)
+// // }
+
+//exerise 6
+// func main() {
+
+// 	participants := 42
+// 	groupCount := 8
+
+// 	participantsPerGroup := participants / groupCount
+
+// 	fmt.Println(participantsPerGroup)
+// }
+
+//exercise 7
+// func main() {
+
+// 	fmt.Println(20 - 4*3)
+// 	fmt.Println((20 - 4) * 3) // выводится разный ответ так как GO использует мат. последовательность
+// }
+
+//exercise 8
+
+// func main() {
+
+// 	squareValue := 81
+
+// 	result := math.Sqrt(float64(squareValue))
+
+// 	fmt.Println(result)
+
+// 	multiplier := 5
+// 	exponent := 2
+
+// 	result2 := math.Pow(float64(multiplier), float64(exponent))
+
+// 	fmt.Println(result2)
+// }
